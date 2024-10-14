9032d9308e5fa4eb1eedaacf012db477c6708385
Davit Munjishvili <dmunjishvili@impel.ai>
2024-10-14 22:40:45 +0400

    display tabbar

--- wezterm.lua
+++ wezterm.lua
@@ -13,9 +13,16 @@ config.font_size = 24
 config.macos_window_background_blur = 20
 
 config.window_background_image_hsb = {}
-config.enable_tab_bar = false
 config.native_macos_fullscreen_mode = true
 config.disable_default_key_bindings = true
+
+config.show_tab_index_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 64
+
+config.tab_bar_style = {}
 
 wezterm.on("gui-startup", function()
 	local _, pane, window = mux.spawn_window({})
