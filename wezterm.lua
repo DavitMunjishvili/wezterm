32acbf4d44294995d4f1eccf8385bee21854abf8
Davit Munjishvili <dmunjishvili@impel.ai>
2024-11-23 19:26:56 +0400

    up changes

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require("wezterm")
-local mux = wezterm.mux
-local config = {}
+local config = wezterm.config_builder()
 
 config.color_scheme = "GruvboxDark"
+
+-- config.window_background_image = "/Users/dmunjishvili/.config/wezterm/powered-by.png"
+
 config.font = wezterm.font({
 	family = "JetBrainsMono Nerd Font",
 	weight = "Regular",
@@ -10,26 +12,67 @@ config.font = wezterm.font({
 })
 config.font_size = 24
 
-config.macos_window_background_blur = 20
-
-config.window_background_image_hsb = {}
 config.native_macos_fullscreen_mode = true
-config.disable_default_key_bindings = true
-
-config.show_tab_index_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
 config.show_new_tab_button_in_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
-config.tab_max_width = 64
 
-config.tab_bar_style = {}
+-- config.colors = {
+-- 	tab_bar = {
+-- 		inactive_tab_edge = "transparent",
+-- 	},
+-- }
 
-wezterm.on("gui-startup", function()
-	local _, pane, window = mux.spawn_window({})
-	local gui_window = window:gui_window()
-	gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
-end)
+-- local function tab_title(tab_info)
+-- 	local title = tab_info.tab_title
+--
+-- 	if title and #title > 0 then
+-- 		return title
+-- 	end
+--
+-- 	return tab_info.active_pane.title
+-- end
+--
+-- wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+-- 	local edge_background = "transparent"
+-- 	local background = "#282828"
+-- 	local foreground = "#fabd2f"
+--
+-- 	if tab.is_active then
+-- 		background = "#fabd2f"
+-- 		foreground = "#282828"
+-- 	elseif hover then
+-- 		background = "#504945"
+-- 	end
+--
+-- 	local edge_foreground = background
+--
+-- 	local title = tab_title(tab)
+--
+-- 	title = wezterm.truncate_right(title, max_width - 2)
+--
+-- 	return {
+-- 		{ Background = { Color = edge_background } },
+-- 		{ Foreground = { Color = edge_foreground } },
+-- 		{ Text = SOLID_LEFT_ARROW },
+-- 		{ Background = { Color = background } },
+-- 		{ Foreground = { Color = foreground } },
+-- 		{ Text = title },
+-- 		{ Background = { Color = edge_background } },
+-- 		{ Foreground = { Color = edge_foreground } },
+-- 		{ Text = SOLID_RIGHT_ARROW },
+-- 	}
+-- end)
 
+-- wezterm.on("gui-startup", function(cmd)
+-- 	local _, _, window = mux.spawn_window(cmd or { workspace = "account management" })
+-- 	local _, account_management_nvim, _ = window:spawn_tab({ cwd = "~" })
+-- 	account_management_nvim:send_text("cd Codes/account-management/\n")
+-- 	window:gui_window():toggle_fullscreen()
+-- end)
+
+config.disable_default_key_bindings = true
 config.keys = {
 	{ key = "Enter", mods = "CMD", action = wezterm.action.ToggleFullScreen },
 	{ key = "+", mods = "CMD", action = wezterm.action.IncreaseFontSize },
@@ -46,6 +89,8 @@ config.keys = {
 	{ key = "p", mods = "CMD|SHIFT", action = wezterm.action.ActivateCommandPalette },
 	{ key = "h", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Left") },
 	{ key = "l", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "j", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Up") },
 	{
 		key = "w",
 		mods = "CMD|SHIFT",
